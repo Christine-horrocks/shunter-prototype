7f25fa60450c71f20a7aeb89f9a0f312e0b85389
Allan Wazacz <cazwazacz@gmail.com>
2018-05-18 17:06:40 +0100

    Create and use walk helper

--- test/integration/home_page/home_page_test.js
+++ test/integration/home_page/home_page_test.js
@@ -1,24 +1,14 @@
 var assert = require('assert');
 var shunterTestHelper = require('shunter').testhelper();
 var fixtureHelper = require('../../helpers/fixture-helper');
+var paths = require('../../helpers/walk-helper');
 
 var passed_json = fixtureHelper.getJSONFixture('/Users/Allan/parliament/shunter-prototype/test/fixtures/json/home_page/home_page.json');
 var expected_html = fixtureHelper.getHTMLFixture('/Users/Allan/parliament/shunter-prototype/test/fixtures/html/home_page/home_page.html');
 
 describe('Home page', function() {
     before(function() {
-        shunterTestHelper.setup(
-            '../../../view/layout.dust',
-            '../../../view/components/cookie-banner.dust',
-            '../../../view/components/banner.dust',
-            '../../../view/components/header.dust',
-            '../../../view/components/hero.dust',
-            '../../../view/components/home-page-subheading.dust',
-            '../../../view/components/list-component.dust',
-            '../../../view/components/highlight-component.dust',
-            '../../../view/components/content.dust',
-            '../../../view/components/footer.dust'
-        ); 
+        shunterTestHelper.setup(paths); 
     });
 
     after(shunterTestHelper.teardown);
