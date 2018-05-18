592a4ee52dfd5f3ba3654a04fb464e1884d1ebc4
Allan Wazacz <cazwazacz@gmail.com>
2018-05-18 17:30:45 +0100

    Fixture helpers get file paths from callers

--- test/helpers/fixture-helper.js
+++ test/helpers/fixture-helper.js
@@ -1,10 +1,28 @@
 var fs = require('fs');
+var callsites = require('callsites');
+
+function jsonFixturePath(path) {
+    var split_path = path.split('/');
+    split_path.splice(-1, 1);
+    split_path[split_path.length - 2] = 'fixtures/json';
+    return split_path.join('/');
+}
+
+function htmlFixturePath(path) {
+    var split_path = path.split('/');
+    split_path.splice(-1, 1);
+    split_path[split_path.length - 2] = 'fixtures/html';
+    return split_path.join('/');
+}
 
 module.exports = {
-    getJSONFixture: function(path) {
-        return JSON.parse(fs.readFileSync(path, 'utf8'));
+    getJSONFixture: function(file_name) {
+        var file_path = jsonFixturePath(callsites()[1].getFileName()) + '/' + file_name + '.json';
+        return JSON.parse(fs.readFileSync(file_path, 'utf8'));
     },
-    getHTMLFixture: function(path) {
-        return fs.readFileSync(path, 'utf8');
+
+    getHTMLFixture: function(file_name) {
+        var file_path = htmlFixturePath(callsites()[1].getFileName()) + '/' + file_name + '.html';
+        return fs.readFileSync(file_path, 'utf8');
     }
 }
--- test/integration/home_page/home_page_test.js
+++ test/integration/home_page/home_page_test.js
@@ -3,8 +3,9 @@ var shunterTestHelper = require('shunter').testhelper();
 var fixtureHelper = require('../../helpers/fixture-helper');
 var paths = require('../../helpers/walk-helper');
 
-var passed_json = fixtureHelper.getJSONFixture('/Users/Allan/parliament/shunter-prototype/test/fixtures/json/home_page/home_page.json');
-var expected_html = fixtureHelper.getHTMLFixture('/Users/Allan/parliament/shunter-prototype/test/fixtures/html/home_page/home_page.html');
+var json_fixture = fixtureHelper.getJSONFixture('home_page');
+var expected_html = fixtureHelper.getHTMLFixture('home_page');
+// var expected_html = fixtureHelper.getHTMLFixture('/Users/Allan/parliament/shunter-prototype/test/fixtures/html/home_page/home_page.html');
 
 describe('Home page', function() {
     before(function() {
@@ -14,7 +15,7 @@ describe('Home page', function() {
     after(shunterTestHelper.teardown);
 
     it('should render the homepage', function(done) {
-        shunterTestHelper.render('layout', passed_json, function(error, dom, output) {
+        shunterTestHelper.render('layout', json_fixture, function(error, dom, output) {
             assert.strictEqual(expected_html, output);
             done();
         });
